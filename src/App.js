5383ddd140f26fb55ae0fc540eaa401dedb64183
Anshu Thopsee <thopseeanshu@gmail.com>
2023-04-18 00:34:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Home from "./Home.js";
 
 function App() {
 
   const [files, setFiles] = useState([]);
-  const [popup, setPopup] = useState({show: false, message: ""});
+  const [popup, setPopup] = useState({show: false, message: "", timeout: 0});
 
   return (
     <div className="App">
       <div className='navbar'>
         <span className='header'>image2pdf</span>
+        <a href="https://github.com/anshuthopsee/image2pdf">Github repo</a>
       </div>
       <Home setFiles={setFiles} files={files} popup={popup} setPopup={setPopup}/>
     </div>
