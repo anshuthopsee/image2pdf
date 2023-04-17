ef61e0c651c06671f26d43c064563044ba661dfc
Anshu Thopsee <thopseeanshu@gmail.com>
2023-04-18 00:37:05 +0100

    Update Info.js

--- src/Info.js
+++ src/Info.js
@@ -5,10 +5,10 @@ const Info = () => {
                 upto 20 images. JPEG/PNG image types supported. GIFS not supported.
             </div>
             <h3 className="info">Instructions</h3>
-            <div className="info">First, upload one or upto 20 images, by clicking on the dark grey, or by dragging an dropping your images onto it.</div>
+            <div className="info">First, upload one or upto 20 images, by clicking on the blackish area, or by dragging and dropping your images onto it.</div>
             <div className="info">Once uploaded, you'll see the images in the carousel, in the order you've uploaded them. Incase you want to change 
-            the order of the images, just click/touch and hold on the image, and move it around to change its position.</div>
-            <div className="info">Once, you have them in the desired order, you can click on the download button, to download the images as a PDF.</div>
+            the order of the images, just click and hold the image, to move it around and change it's position.</div>
+            <div className="info">Once, you have them in your desired order, you can click on the download button, to download the images as a PDF.</div>
         </div>
     );
 };
