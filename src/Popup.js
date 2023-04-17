a022c26a668e87f1f933f9fbbd08bcd2d04783ec
Anshu Thopsee <thopseeanshu@gmail.com>
2023-04-18 00:36:44 +0100

    Update Popup.js

--- src/Popup.js
+++ src/Popup.js
@@ -3,19 +3,20 @@ import { useEffect } from "react";
 const Popup = (props) => {
 
     useEffect(() => {
-        let timeout = setTimeout(() => {
-            props.setPopup(() => { return {show: false, message: ""}});
-        }, 5000);
+        let timer = setTimeout(() => {
+            props.setPopup(() => { return {show: false, message: "", timeout: 0}});
+        }, props.timeout*1000);
 
         return () => {
-            clearTimeout(timeout);
+            clearTimeout(timer);
         };
+
     }, [props.show, props.children]);
 
     return (
-            <div className="popup_container">
-                <div className="popup" style={{borderBottom: "5px solid #24d654"}}>{props.children}</div>
-            </div>
+        <div className="popup_container">
+            <div className="popup" style={{borderBottom: "5px solid #24d654"}}>{props.children}</div>
+        </div>
     );
 };
 
