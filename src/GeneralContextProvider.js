1b22bbbe83ad6e70527edd3ec181a524c75c7df9
Anshu Thopsee <thopseeanshu@gmail.com>
2023-04-25 23:40:17 +0100

    Update GeneralContextProvider.js

--- src/GeneralContextProvider.js
+++ src/GeneralContextProvider.js
@@ -5,7 +5,6 @@ export const GeneralContext = createContext();
 const GeneralContextProvider = ({ children }) => {
     const [files, setFiles] = useState([]);
     const [popup, setPopup] = useState({show: false, message: "", timeout: 0});
-    const [chosen, setChosen] = useState({element: null, offsetX: null, offsetY: null, left: null, top: null});
 
     return (
         <GeneralContext.Provider value={{files, setFiles, popup, setPopup, chosen, setChosen}}>
