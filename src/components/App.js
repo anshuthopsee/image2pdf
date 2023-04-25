8f076c9bab612df4cbd3970995bbdfeb38727823
Anshu Thopsee <thopseeanshu@gmail.com>
2023-04-25 23:43:33 +0100

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import GeneralContextProvider from "./GeneralContextProvider.js";
+import Home from "./components/Home.js";
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='navbar'>
+        <span className='header'>image2pdf</span>
+        <a href="https://github.com/anshuthopsee/image2pdf">GitHub repo</a>
+      </div>
+      <GeneralContextProvider>
+        <Home/>
+      </GeneralContextProvider>
+    </div>
+  );
+}
+
+export default App;
